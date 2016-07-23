374b556b815c87ae128ba1379df0c294b289fd73
DBJ <786223690@qq.com>
2016-07-23 09:54:48 +0800

    index.php提交

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+    define('HTML_CACHE_ON',false); // 关闭静态缓存
+    define('APP_DEBUG',false);
+    define('BIND_MODULE','Home');
+    //define('BUILD_MODEL_LIST','User,Menu');
+    define('APP_PATH','./admin/');
+    //define('THINK_PATH','./ThinkPHP_3.2/');
+    require './ThinkPHP_3.2/ThinkPHP.php';
+    
+?>
